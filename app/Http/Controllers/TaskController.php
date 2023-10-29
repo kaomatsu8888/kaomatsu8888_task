1f41d7f2a8934130991c70abedc9d97ce9f0f9ef
kaomatsu8888 <m.kaoru0830@gmail.com>
2023-10-29 23:46:39 +0900

    third commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+// Taskモデルを呼び出す
+use App\Models\Tasks;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    // indexメソッドを定義
+    public function index()
+    {   // Tasksテーブルの全てのデータを取得
+        $tasks = Tasks::all();
+        // tasks.indexビューを表示
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+
+    // showメソッドを定義
+    public function show($id)
+    {
+        $task = Tasks::find($id);
+        return view('tasks.show', ['task' => $task]);
+}
 }
