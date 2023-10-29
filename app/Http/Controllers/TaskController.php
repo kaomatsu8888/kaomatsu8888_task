1a11de059806c38a00acb464c83339955c8c079c
kaomatsu8888 <m.kaoru0830@gmail.com>
2023-10-30 00:22:46 +0900

    fiveth commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 // Taskモデルを呼び出す
 use App\Models\Tasks;
 use Illuminate\Http\Request;
+use Termwind\Components\Raw;
 
 class TaskController extends Controller
 {
@@ -43,5 +44,28 @@ class TaskController extends Controller
     {
         $task = Tasks::find($id);
         return view('tasks.show', ['task' => $task]);
-}
+    }
+    // editメソッドを定義
+    public function edit($id)
+    {
+        $task = Tasks::find($id);
+        return view('tasks.edit', ['task' => $task]);
+    }
+    // updateメソッドを定義
+    public function update(Request $request, $id)
+    {
+        $task = Tasks::find($id);
+        $task->title = $request->title;
+        $task->body = $request->body;
+        $task->save();
+        return redirect('/tasks');
+    }
+
+    // destroyメソッドを定義
+    public function destroy($id)
+    {
+        $task = Tasks::find($id);
+        $task->delete();
+        return redirect('/tasks');
+    }
 }
